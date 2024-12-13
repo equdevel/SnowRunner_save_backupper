7080a5d216c76f21b8d5c3329b900bf5d2ec4a25
equdevel <equdevel@gmail.com>
2024-12-13 13:43:40 +0800

    add Expeditions support

---
+++ Expeditions/RESTORE SAVES.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+set ROOT=%CD%\..
+
+call %ROOT%\exe\env.cmd Expeditions
+
+rmdir /s /q "%SAVE_DIR%" 2> NUL
+xcopy .\_backup "%SAVE_DIR%" /e /q /r /y /v /i
+
+rem rmdir /s /q .\_restore 2> NUL
+rem xcopy .\_backup .\_restore /e /q /r /y /v /i
+
+echo.
+pause
